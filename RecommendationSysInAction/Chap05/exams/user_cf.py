2ffa2e96fd8ab1f6bc56cc24c0ec281f359dc076
赵成明 <zhaochengming@meituan.com>
2019-09-16 19:27:42 +0800

    adding user-cf for movie-lens Signed-off-by: 赵成明 <zhaochengming@meituan.com>

--- RecommendationSysInAction/Chap05/exams/user_cf.py
+++ RecommendationSysInAction/Chap05/exams/user_cf.py
@@ -4,11 +4,13 @@ import os
 import random
 import math
 import json
-import numpy as np
 import logging
 import pandas as pd
 from tqdm import tqdm
-from typing import List, Dict, Any, Set, Iterable, Tuple
+from collections import defaultdict
+from _utils.ucollections import PriorityQueue
+from _utils.context import timer
+from typing import Dict, Set, Tuple
 
 
 logging.basicConfig(format='%(asctime)s [line: %(lineno)d] - %(levelname)s: %(message)s', level=logging.DEBUG)
@@ -17,41 +19,130 @@ MOVIE_LENS_SRC = r"F:\for learn\Python\推荐系统开发实战\章节代码\dat
 
 
 class UserCFRecommend(object):
-    seed = 0
-    test_size = 0.1
+    tmp_path = os.path.join(os.path.dirname(__file__), "tmp")
+    similarity_file = os.path.join(tmp_path, "user_similarity.json")
 
     def __init__(self, rating_data: pd.DataFrame):
-        self.data = rating_data
-        self.user_index_mapping = self.generate_index_mapping(rating_data["user"].unique())
-        self.item_index_mapping = self.generate_index_mapping(rating_data["item"].unique())
-        self.n_users = len(self.user_index_mapping)
-        self.n_items = len(self.item_index_mapping)
-        self.rng = random.Random(self.seed)
-        self.train_data, self.test_data = self.split(self.data)
+        self.train_data, self.test_data = self.split(rating_data, seed=0, test_size=0.1)
+        self.item_users, self.user_rated_summary, self.train_data = self.generate_data(self.train_data)
+        self.user_sims: Dict[str, Dict[str, float]] = self.cal_user_similarity()  # User rated items summary
 
     @classmethod
     def from_file(cls, data_file: str):
         if not os.path.exists(data_file):
             raise IOError(f"File not found `{data_file}`.")
-        data = pd.read_csv(data_file, sep="::", names=["user", "item", "score", "timestamp"], header=None)
+        LOGGER.info(f"Reading file from {data_file}...")
+        data = pd.read_csv(
+            data_file, sep="::", names=["user", "item", "score", "timestamp"], header=None, engine="python")
+
+        data["user"] = data["user"].astype(str)
+        data["item"] = data["item"].astype(str)
         return cls(data[["user", "item", "score"]])
 
     @staticmethod
-    def generate_index_mapping(seq: Iterable[Any]) -> Dict[Any, int]:
-        return {k: i for i, k in enumerate(seq)}
-
-    def split(self, data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    def split(data: pd.DataFrame, seed: int, test_size: float) -> Tuple[pd.DataFrame, pd.DataFrame]:
         """Group by user to split entries."""
-        is_train = data.groupby("user")["item"]\
-            .transform(lambda s: [self.rng.random() > self.test_size for _ in range(len(s))])
+        LOGGER.info(f"Split data into train and test. Test size `{test_size}`")
+        rng = random.Random(seed)
+        is_train = data.groupby("user")["item"].transform(lambda s: [rng.random() > test_size for _ in range(len(s))])
         return data[is_train], data[~is_train]
 
     @staticmethod
-    def _cal_user_similarity(a: Set[Any], b: Set[Any]) -> float:
+    def generate_data(data: pd.DataFrame) -> Tuple[Dict[str, int], Dict[str, Set[str]], Dict[str, Dict[str, int]]]:
+        """Generate different type of data from given original rating data.
+        :returns: 1) The total count of rated users for each item.
+                  2) The total set of items for each user.
+                  3) The dict-transformed data from original DataFrame.
+        """
+        LOGGER.info("Extract data information...")
+        # Summary of item, the num of users who rated on it.
+        item_users: Dict[str, int] = dict(data["item"].value_counts())
+
+        # Summary of all rated item set for each user
+        user_rated_summary: pd.DataFrame = data.groupby("user").agg({"item": lambda s: set(list(s))})
+        user_rated_summary: Dict[str, Set[str]] = user_rated_summary["item"].to_dict()
+
+        # Transform data from pd.DataFrame to Dict of Dict
+        def generate_record(d: pd.DataFrame):
+            d["record"] = d.apply(lambda row: dict(zip(row["item"], row["score"])), axis=1)
+            return d[["record"]]
+        manifest_data = data.groupby("user").agg({"item": tuple, "score": tuple}).pipe(generate_record)
+        manifest_data = manifest_data["record"].to_dict()
+        return item_users, user_rated_summary, manifest_data
+
+    def _cal_user_similarity(self, a: Set[str], b: Set[str]) -> float:
         """Cosine similarity between two set, each element indicates a dimension"""
-        return len(a & b) / ((len(a) * len(b)) ** 0.5)
+        intersection = a & b
+        x = sum([1 / math.log(self.item_users[item] + 1) for item in intersection])  # adding hot punishment
+        return x / ((len(a) * len(b)) ** 0.5)
 
-    def cal_user_similarity(self):
-        similarity = np.zeros(shape=(self.n_users, self.n_users))
+    def cal_user_similarity(self) -> Dict[str, Dict[str, float]]:
+        if os.path.exists(self.similarity_file):
+            LOGGER.info("Loading similarity from disk...")
+            with open(self.similarity_file, "r") as f:
+                similarity = json.load(f)
+            return similarity
 
-        user_summary = self.train_data.groupby("user").agg({"item": lambda s: set(list(s))})
+        LOGGER.info("Generate new similarity files...")
+        similarity: Dict[str, Dict[str, float]] = defaultdict(dict)
+        user_rated_summary = list(self.user_rated_summary.items())
+        n_users = len(user_rated_summary)
+        for i in tqdm(range(n_users)):
+            u, u_sets = user_rated_summary[i]
+            for j in range(i + 1, n_users):
+                v, v_sets = user_rated_summary[j]
+                similarity[u][v] = similarity[v][u] = self._cal_user_similarity(u_sets, v_sets)
+
+        os.makedirs(self.tmp_path, exist_ok=True)
+        with open(self.similarity_file, "w") as f:
+            json.dump(similarity, f)
+        return similarity
+
+    def recommend(self, user: str, k: int = 8, n_items: int = 40):
+        """Recommend for target user, only consider Top`k` neighbour users and output `n_items` items."""
+        neighbours = sorted(self.user_sims[user].items(), key=lambda pair: pair[1], reverse=True)[:k]
+        rated_items = self.user_rated_summary[user]
+        recall_items = {}
+
+        for user_, sim in neighbours:
+            for item, score in self.train_data[user_].items():
+                if item in rated_items:
+                    continue
+                recall_items.setdefault(item, 0)
+                recall_items[item] += score * sim
+
+        r = PriorityQueue(maxsize=n_items)
+        for item, score in recall_items.items():
+            r.push(score, item)
+
+        return r.queue()
+
+    def evaluation(self, k: int = 8, n_items: int = 10) -> Tuple[float, float]:
+        """Compute precision and recall"""
+        test_user_rated_items: Dict[str, Set[str]] =\
+            self.test_data.groupby("user").agg({"item": lambda s: set(list(s))})["item"].to_dict()
+
+        hit = 0
+        test_num = 0
+        pred_num = 0
+        for test_user, test_items in test_user_rated_items.items():
+            pred_items = self.recommend(test_user, k=k, n_items=n_items)
+            test_num += len(test_items)
+            pred_num += len(pred_items)
+            for _, pred_item in pred_items:
+                if pred_item in test_items:
+                    hit += 1
+
+        recall = hit / test_num
+        precision = hit / pred_num
+        return precision, recall
+
+
+if __name__ == '__main__':
+    rec = UserCFRecommend.from_file(os.path.join(MOVIE_LENS_SRC, "ratings.dat"))
+    with timer("Recommend"):
+        result = rec.recommend("1", k=8, n_items=40)
+    print(result)
+
+    precision, recall = rec.evaluation(k=8, n_items=10)
+    print(f"Precision: {precision}, Recall: {recall}.")  # Precision: 0.17529, Recall: 0.10440.
